2bf01ac2b0fac045f9a252895940e287c499a926
kate-son <yenihand13@gmail.com>
2026-06-17 03:46:57 +0900

    chore: eslint package.json에 설치. eslint.config.ts 추가.
    - JS, TypeScript, React 규칙 적용
    - JSX 상단 import문 작성 규칙 제외
    - rules-of-hooks와 exhaustive-deps error 처리

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+    reactHooks.configs.flat["recommended-latest"],
+    {
+        rules: {
+            "react/react-in-jsx-scope": "off", // React 17+ 에서는 불필요
+            "react-hooks/rules-of-hooks": "error",
+            "react-hooks/exhaustive-deps": "error",
+        }
+    }
+]);
