9560bb850f8a890d944515575ecf0be2c63e5641
kate-son <yenihand13@gmail.com>
2026-06-25 15:43:15 +0900

    chore: eslint 규칙 수정 (no-magic-numbers)

--- eslint.config.js
+++ eslint.config.js
@@ -33,7 +33,10 @@ export default defineConfig([
             "no-console": "warn", //console.log 잔재 확인 : 개발 중 디버깅 용으로 사용할 수 있어 warn으로 처리. (CI/CD 도입 시 error로 분기처리 필요)
             "@typescript-eslint/no-explicit-any": "error", //any 사용 막기 :  any사용시 타입 검사가 없어 ts확인 불가능. 타입 확인 강제하기 위해서 사용
             "@typescript-eslint/ban-ts-comment": "error", //@ts-ignore, @ts-nocheck 같은 주석으로 TypeScript 에러를 숨기는거 방지
-            "no-magic-numbers": "error", //고정값 상수로 관리하도록 하기 위하여 강제
+            "no-magic-numbers": [
+                "error",
+                {"ignore": [0, 1, -1]}
+            ], // 고정값 상수로 관리하도록 강제. 단, 0(초기값/인덱스), 1(증감), -1(indexOf 실패) 제외
             "no-nested-ternary": "error", //삼항 연산자 중첩 사용 시 가독성 저하로 error 처리
             "no-empty": "error", //빈 블록으로 에러를 무시하는 것을 방지
             "no-var": "error", //var 사용 금지, const/let 사용 강제
