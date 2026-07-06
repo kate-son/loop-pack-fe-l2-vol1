01d6d8caf9280645319baf2809605ddbd15a8feb
kate-son <yenihand13@gmail.com>
2026-07-07 02:06:41 +0900

    chore: husky commit-msg/pre-commit 훅 연결 및 pnpm-workspace allowBuilds 수정

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,3 +1,16 @@
 export default {
   extends: ["@commitlint/config-conventional"],
+  ignores: [(commit) => /^Merge branch/.test(commit)],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "chore"],
+    ],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 72],
+  },
 };
